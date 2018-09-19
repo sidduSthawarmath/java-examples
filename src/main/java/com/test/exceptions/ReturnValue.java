34f649e5ccc219e18211062c9c9378ca39a111f9
Siddu <siddusthawarmath@gmail.com>
2018-09-19 13:12:11 +0530

    If method returning value then it should follow 2 rule

---
+++ src/main/java/com/test/exceptions/ReturnValue.java
@@ -0,0 +1,35 @@
+package com.test.exceptions;
+
+public class ReturnValue {
+	public static void main(String[] args) {
+    
+		/*
+		 * If method has return type then 
+		 * 
+		 * 1.If finally block returns a value
+		 * then try and catch blocks may or may not return a value 
+		 * 
+		 * 2.If finally
+		 * block does not return a value then both try and catch blocks must
+		 * return a value.
+		 */
+		int val = retunValue();
+		System.out.println("Value:" + val);
+	}
+
+	private static int retunValue() {
+		try {
+			int i = 100 / 0;
+			System.out.println("In try block");
+			return 1;   //It should return
+		} catch (Exception e) {
+			System.out.println("In catch block");
+			return 2;  //It should return
+		} finally {
+			System.out.println("In finally block");
+			// return 3;   //We don't have return in finally so try and catch both are returning value  
+
+		}
+
+	}
+}
