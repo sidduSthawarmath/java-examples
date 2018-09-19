e95992480e97bd9ec71373704fd1f12a377bb8da
Siddu <siddusthawarmath@gmail.com>
2018-09-19 12:45:48 +0530

    Nested try catch block example

--- src/main/java/com/test/exceptions/NestedTryBlock.java
+++ src/main/java/com/test/exceptions/NestedTryBlock.java
@@ -3,10 +3,10 @@ package com.test.exceptions;
 public class NestedTryBlock {
 	public static void main(String[] args) {
 		try {
-			Integer i = new Integer("100");
-			try {
+			Integer i = new Integer("100"); //chance of NumberFormatException
+			try {     //This is nested try block                 
 				String name = null;
-				System.out.println(name.equals("sidddu"));
+				System.out.println(name.equals("sidddu"));  //NullPointerException
 			} catch (NullPointerException e) {
 				e.printStackTrace();
 			}
