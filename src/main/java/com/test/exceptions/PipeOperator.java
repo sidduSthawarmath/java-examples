6a7d68e945743d19af3b20e29f6334dbcac47ba4
Siddu <siddusthawarmath@gmail.com>
2018-09-19 12:27:20 +0530

    try block can be handled by a single catch block using pipe (|) operator

---
+++ src/main/java/com/test/exceptions/PipeOperator.java
@@ -0,0 +1,21 @@
+package com.test.exceptions;
+
+/*
+ * From Java 7 onward, there is one more way for handling multiple
+ * exceptions. Multiple exceptions thrown by the try block can be handled by
+ * a single catch block using pipe (|) operator. By using pipe operator
+ */
+public class PipeOperator {
+
+	public static void main(String[] args) {
+
+		try {
+
+			int i=100/0;
+			
+		} catch (NullPointerException | ArithmeticException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
