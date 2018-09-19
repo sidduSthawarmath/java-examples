11abc3c22983fcad8f30fa4b61d9626d222933fb
Siddu <siddusthawarmath@gmail.com>
2018-09-19 17:41:24 +0530

    Checked exception

--- src/main/java/com/test/exceptions/CustomException.java
+++ src/main/java/com/test/exceptions/CustomException.java
@@ -1,17 +1,20 @@
 package com.test.exceptions;
 
 public class CustomException {
-
 	public static void main(String[] args) throws BalanceException {
 		int balance = -100;
 		if (balance < 0) {
-			throw new BalanceException("Balance should not be in negative");  //Throw custom exception
+			
+			// throwing checked exception(because BalanceException extends
+			// Exception)
+			// So we should handle using try/catch or we should use throws
+	   		
+			throw new BalanceException("Balance should not be in negative");
 		}
 	}
 }
 
-
-//This custom exception
+// This custom exception
 class BalanceException extends Exception {
 	public BalanceException(String msg) {
 		super(msg);
